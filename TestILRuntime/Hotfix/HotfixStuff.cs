c0f9ae03ea0e2e05dd82c95e47c0b058d9a64cb9
DiracseaFish <602633512@qq.com>
2023-09-05 16:14:03 +0800

    init

---
+++ TestILRuntime/Hotfix/HotfixStuff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Hotfix
+{
+    public class HotfixStuff
+    {
+        public static void TestFunc1()
+        {
+            Debug.Log("Run Hotfix HotfixStuff TestFun1");
+        }
+    }
+}
